8afffb88b4581e143b461e3dce8de3ac749d24cf
Yihun Alemayehu <yankure01@gmail.com>
2024-01-10 21:24:50 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Payment'),
+        centerTitle: true,
       ),
       body: Container(),
     );
